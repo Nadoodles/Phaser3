60f6d5ce32132c76ec3365ccae67df790f40896b
Nadoodles <43650566+Nadoodles@users.noreply.github.com>
2023-06-12 23:44:25 -0700

    fire collision and spawn complete

--- GraveOfTheFireFlies/src/prefabs/Seita.js
+++ GraveOfTheFireFlies/src/prefabs/Seita.js
@@ -11,21 +11,17 @@ class Seita extends Phaser.Physics.Arcade.Sprite {
         // left/right movement
         if (keyLEFT.isDown && this.x > borderUISize) {
             this.x -= this.moveSpeed; 
-            //this.duckSound.play();
         }
         else if (keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width) {
             this.x += this.moveSpeed;
-            //this.duckSound.play();
         }
 
         // up/down movement
         if (keyUP.isDown && this.y >= borderUISize) {
             this.y -= this.moveSpeed; 
-            //this.duckSound.play();
         }
         else if (keyDOWN.isDown && this.y <= game.config.height - borderUISize - this.height) {
             this.y += this.moveSpeed; 
-            //this.duckSound.play();
         }
 
     }
